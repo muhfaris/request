6b26188a1deab00cd70358340ab2880f1c482dd7
Muhammad Faris <muhfaris@disroot.org>
2020-05-15 18:08:07 +0700

    add mime type

---
+++ commons.go
@@ -0,0 +1,12 @@
+package request
+
+const (
+	// MimeTypeJSON is application/json type
+	MimeTypeJSON = "application/json"
+
+	// MimeTypeFormData is application/form-data
+	MimeTypeFormData = "multipart/form-data"
+
+	// MimeTypeFormUrl is application/x-www-form-urlencoded type
+	MimeTypeFormUrl = "application/x-www-form-urlencoded"
+)

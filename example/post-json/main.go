9faa4f6b02dd5f97f3b5ded289a1a10b8e78952b
Muhammad Faris <muhfaris@disroot.org>
2022-06-21 21:58:20 +0700

    feat: new example

---
+++ example/post-json/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/muhfaris/request"
+)
+
+type User struct {
+	Name string
+	Job  string
+}
+
+func main() {
+	user := User{
+		Name: "faris",
+		Job:  "software developer",
+	}
+
+	raw, err := json.Marshal(user)
+	if err != nil {
+		log.Printf("error marshal the user data, %v", err)
+		return
+	}
+
+	config := &request.Config{
+		URL:         "https://reqres.in/api/users",
+		Body:        raw,
+		ContentType: "application/json",
+	}
+
+	resp := request.Post(config)
+	if resp.Error != nil {
+		log.Printf("error create new post user, %v", resp.Error)
+		return
+	}
+
+	log.Println("Resp")
+	log.Println(string(resp.Body))
+}
